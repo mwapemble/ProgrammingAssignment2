7738d0f0f0e05899d7ae2fb67bacd9e62bdefe8f
Matthew Pemble <matthew@pemble.net>
2015-07-20 17:12:24 +0100

    Update cachematrix.R
    Modified matrix operating functions:
    
    Test results:
    
    ## set up a random 3x3 matrix. 
    > temp2 <- matrix(rnorm(9),3,3)
    
    ## initialise the matrix list through the makeCacheMatrix function
    > temp <- makeCacheMatrix(temp2)
    
    ## solve the matrix list for the first time - the inverse object should be NULL
    > cacheSolve(temp)
                [,1]        [,2]        [,3]
    [1,]  0.66247654 -0.09588692  0.23228597
    [2,] -0.03876314 -0.70896297 -0.05212145
    [3,] -0.24809413 -0.23367019  0.80754429
    ## okay, it had an inverse. That's a good thing.
    
    ## solve the matrix for the second time - should get same result but prepended with the cache message.
    > cacheSolve(temp)
    getting cached data
                [,1]        [,2]        [,3]
    [1,]  0.66247654 -0.09588692  0.23228597
    [2,] -0.03876314 -0.70896297 -0.05212145
    [3,] -0.24809413 -0.23367019  0.80754429
    
    ## Stunning!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is an answer to the 2nd Programming Assignment for the R Programming course
+## Lexical Scoping
+## It uses as a base the example functions provided in the assignment
+## But modifies them slightly to perform matrix operations - inversion, cache 
+## & storage, rather than taking the mean of a vector.
 
-## Write a short comment describing this function
+## Takes a matrix, x, and sets it in to the parent environment as a list of functions
+## Initially, the inverse is NULL
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Initialise variable
+  i <- NULL
+  
+  ## set sub.function sets value into parent environment variable x and NULL in to parent environment variable y
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function operates on a list object created by makeCacheMatrix
+## It checks for a cached inverse and returns it if it exists.
+## Otherwise it calculates the inverse, caches it and returns it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  ## Fetch the possibly NULL inverse object from the supplied list.
+  i <- x$getinverse()
+  
+  ## Test to see if cached inverse is not NULL
+  if(!is.null(i)) {
+    ## Return cache message then cached answer
+    message("getting cached data")
+    i
+  }
+  else
+  {
+    ## Fetch matrix from list
+    data <- x$get()
+    ## Solve matrix using standard function
+    i <- solve(data)
+    ## Set inverse in to list
+    x$setinverse(i)
+    ## return answer
+    i
+  }  
 }
